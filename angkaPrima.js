d03375fe10350a9855df335d51cc87add68553c6
jokohijrawanto <biterasystem@gmail.com>
2020-04-07 23:50:27 +0700

    1

---
+++ angkaPrima.js
@@ -0,0 +1,26 @@
+function angkaPrima(angka) {
+  // you can only write your code here!
+
+    var max = Math.sqrt(angka);
+    for( var i = 2;  i <= max;  i++ ) {
+        if( angka % i == 0 )
+            return false;
+    }
+    return true;
+}
+
+for( var angka = 1;  angka< 20;  angka++ ) {
+        if( angkaPrima(angka)) {
+        
+ }
+}
+
+
+
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
