d83129856adaddbb15374cb5ca32c7bc0ac26a78
jokohijrawanto <biterasystem@gmail.com>
2020-04-08 01:25:26 +0700

    median

---
+++ median.js
@@ -0,0 +1,24 @@
+function cariMedian(arr) {
+  // you can only write your code here!
+
+  arr.sort( function(a,b) {return a + b;} );
+
+  var half = Math.floor(arr.length/2);
+
+  if(arr.length % 2)
+      return arr[half];
+  else
+      return (arr[half-1] + arr[half]) / 2;
+}
+
+
+// TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
+
+
+
+
